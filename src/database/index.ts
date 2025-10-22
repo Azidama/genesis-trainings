f8508d758b21432aa4621a7d60a6670d8d0bc47b
azidama <61466711+Azidama@users.noreply.github.com>
2025-10-22 15:57:18 -0400

    bring back migrations

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,5 +3,10 @@ import { seedDb } from '@/scripts/seed'
 
 export const dbConnection = async () => {
   await AppDataSource.initialize()
+  try {
+    await AppDataSource.runMigrations()
+  } catch (error) {
+    console.log("Error while running migrations: ", error)
+  }
   await seedDb()
 }
